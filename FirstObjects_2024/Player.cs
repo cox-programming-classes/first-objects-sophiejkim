9a3c07d595dc2c540c40dc25db809fc940645b26
sophiejkim <158072125+sophiejkim@users.noreply.github.com>
2024-02-25 22:53:54 -0500

    PLAYER

--- FirstObjects_2024/Player.cs
+++ FirstObjects_2024/Player.cs
@@ -2,7 +2,13 @@ namespace FirstObjects_2024;
 
 
 public class Player
-{ 
-   private List<hand> hands;
-   
+{
+   private hand _hand = new();
+
+}
+
+//*NEED HELP : HOW TO MAKE VISIBLE OUTSIDE OF CLASS
+public bool DidStand {
+   EnvironmentVariableTarget; private set; } = false;
+
 }
--- FirstObjects_2024/hand.cs
+++ FirstObjects_2024/hand.cs
@@ -34,5 +34,6 @@ public class hand //: IEnumerable<Card>
       return card;
    }
 
-
+   
+ 
 }
